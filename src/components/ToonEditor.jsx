a735c99444ebdfd882701e359f35922d40b086cd
Myriam Luce <luce.myriam@gmail.com>
2020-10-16 12:47:20 -0400

    Made ToonEditor extend Form component

--- src/components/ToonEditor.jsx
+++ src/components/ToonEditor.jsx
@@ -1,132 +1,64 @@
 import React from "react";
+import * as yup from "yup";
 import "bootstrap/dist/css/bootstrap.css";
+import Form from "./Form";
+import ButtonedNumber from "./ButtonedNumber";
 
-function ToonEditor(props) {
-  const nameId = "name";
-  const physicalId = "physical";
-  const magicalId = "magical";
-  const leadershipId = "leadership";
-  const traitId = "trait";
-  const main = "main_hand";
-  const off = "off_hand";
-  const armor = "armor";
-  return (
-    <form>
-      <div className="form-group my-2">
-        <label htmlFor={nameId}>Name</label>
-        <input
-          type="text"
-          className="form-control"
-          id={nameId}
-          name={nameId}
-          placeholder="Enter your toon's name"
-        />
-      </div>
-      <div className="form-row my-2">
-        <div className="col">
-          <label htmlFor={physicalId}>Physical</label>
+class ToonEditor extends Form {
+  state = {
+    data: {},
+    errors: {},
+  };
+
+  schema = yup.object().shape({});
+
+  async doSubmit() {
+    if (Object.keys(this.state.errors).length === 0) {
+      console.log("submitting...");
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {this.renderInput("text", "name", {
+          placeholder: "Zename Ofzetoon...",
+        })}
+        <ButtonedNumber id="physical" label="Physical" />
+        <ButtonedNumber id="magical" label="Magical" />
+        <ButtonedNumber id="leadership" label="Leadership" />
+        <div className="form-group my-2">
+          <label htmlFor="trait">Trait</label>
+          <select className="form-control" id="trait" name="trait">
+            <option>Resonant</option>
+            <option>Glass Canon</option>
+          </select>
         </div>
-        <div className="col">
-          <button type="button" className="btn btn-primary">
-            -
-          </button>
+        <div className="form-group my-2">
+          <label htmlFor="armor">Armor</label>
+          <select className="form-control" id="armor" name="armor">
+            <option>None</option>
+            <option>Light</option>
+          </select>
         </div>
-        <div className="col">
-          <input
-            type="number"
-            id={physicalId}
-            name={physicalId}
-            className="form-control"
-            readOnly
-          />
+        <div className="form-group my-2">
+          <label htmlFor="main">Main Hand</label>
+          <select className="form-control" id="main" name="main">
+            <option>Grimoire</option>
+            <option>Staff</option>
+          </select>
         </div>
-        <div className="col">
-          <button type="button" className="btn btn-primary">
-            +
-          </button>
+        <div className="form-group my-2">
+          <label htmlFor="off">Off Hand</label>
+          <select className="form-control" id="off" name="off">
+            <option>Empty</option>
+            <option>Unarmed</option>
+          </select>
         </div>
-      </div>
-      <div className="form-row my-2">
-        <div className="col">
-          <label htmlFor={magicalId}>Magical</label>
-        </div>
-        <div className="col">
-          <button type="button" className="btn btn-primary">
-            -
-          </button>
-        </div>
-        <div className="col">
-          <input
-            type="number"
-            id={magicalId}
-            name={magicalId}
-            className="form-control"
-            readOnly
-          />
-        </div>
-        <div className="col">
-          <button type="button" className="btn btn-primary">
-            +
-          </button>
-        </div>
-      </div>
-      <div className="form-row my-2">
-        <div className="col">
-          <label htmlFor={leadershipId}>Leadership</label>
-        </div>
-        <div className="col">
-          <button type="button" className="btn btn-primary">
-            -
-          </button>
-        </div>
-        <div className="col">
-          <input
-            type="number"
-            id={leadershipId}
-            name={leadershipId}
-            className="form-control"
-            readOnly
-          />
-        </div>
-        <div className="col">
-          <button type="button" className="btn btn-primary">
-            +
-          </button>
-        </div>
-      </div>
-      <div className="form-group my-2">
-        <label htmlFor={traitId}>Trait</label>
-        <select className="form-control" id={traitId} name={traitId}>
-          <option>Resonant</option>
-          <option>Glass Canon</option>
-        </select>
-      </div>
-      <div className="form-group my-2">
-        <label htmlFor={armor}>Armor</label>
-        <select className="form-control" id={armor} name={armor}>
-          <option>None</option>
-          <option>Light</option>
-        </select>
-      </div>
-      <div className="form-group my-2">
-        <label htmlFor={main}>Main Hand</label>
-        <select className="form-control" id={main} name={main}>
-          <option>Grimoire</option>
-          <option>Staff</option>
-        </select>
-      </div>
-      <div className="form-group my-2">
-        <label htmlFor={off}>Off Hand</label>
-        <select className="form-control" id={off} name={off}>
-          <option>Empty</option>
-          <option>Unarmed</option>
-        </select>
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Save
-      </button>
-    </form>
-  );
+        {this.renderButton("Save")}
+      </form>
+    );
+  }
 }
 
 export default ToonEditor;
